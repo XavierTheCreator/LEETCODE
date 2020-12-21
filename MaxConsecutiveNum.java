ea297e4cdbfc00d54e01a165fe200a17ca620233
Xavier <52459675+XavierTheCreator@users.noreply.github.com>
2020-12-21 11:48:03 -0500

    Update MaxConsecutiveNum.java

--- MaxConsecutiveNum.java
+++ MaxConsecutiveNum.java
@@ -1,6 +1,6 @@
 /*
- *485. MAX CONSECUTIVES ONES 
- *Given a binary array, find the maximum number of consecutive 1s in this array.
+ * 485. MAX CONSECUTIVES ONES 
+ * Given a binary array, find the maximum number of consecutive 1s in this array.
  *
  *
  *   Solved in O(n)
